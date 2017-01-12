2a81e4da43c27257dc4aa133df9472d5e64136d7
almikola <almikola@gmail.com>
2017-01-12 15:32:15 +0000

    working on validation

--- picasso/config/routes.js
+++ picasso/config/routes.js
@@ -3,6 +3,14 @@ const router = express.Router();
 
 const staticsController = require('../controllers/statics');
 const artController = require('../controllers/arts');
+const authentications = require('../controllers/authenticastions');
+
+router.route('/register')
+  .post(authentications.register);
+
+router.route('/login')
+  .post(authentications.login);
+
 
 router.route('/')
   .get(staticsController.home);
--- picasso/index.js
+++ picasso/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const expressJWT = require('express-jwt');
 const routes = require('./config/routes');
 const port = process.env.PORT || 3000;
 
+const config = require('./config/config');
 const databaseURL = process.env.MONGOLAB_URL || 'mongodb://localhost:27017/art-thiefs';
 
 mongoose.connect(databaseURL);
@@ -12,5 +14,19 @@ mongoose.connect(databaseURL);
 app.use(express.static(`${__dirname}/public`));
 app.use(cors());
 app.use('/', routes);
+
+app.use('/api', expressJWT({ secret: config.secret })
+  .unelss({
+    path: [
+      { url: '/api/register', methods: ['POST'] },
+      { url: '/api/login', methods: ['POST'] }
+    ]
+  }));
+app.use(jwtErrorHandler);
+
+function jwtErrorHandler(err, req, res, next) {
+  if (err.name !== 'UnauthorizedError') return next();
+  return res.status(401).json({ message: 'Unauthorized request.' });
+}
 
 app.listen(port, console.log(`Server has started on port: ${port}`));
