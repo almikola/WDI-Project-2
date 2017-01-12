4a13e8b5625cfec89729d26187b78a86cb914294
almikola <almikola@gmail.com>
2017-01-12 17:45:29 +0000

    started scrapping document

---
+++ picasso/scraping-index.js
@@ -0,0 +1,5 @@
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
+const chalk = require('chalk');
