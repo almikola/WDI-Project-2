74f8039df4e2ebc6858723a1c803d78fba008604
almikola <almikola@gmail.com>
2017-01-18 10:08:29 +0000

    Adding process.env

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: 'mongodb://localhost:27017/art-thiefs',
+  db: process.env.MONGODB_URI || 'mongodb://localhost:27017/art-thiefs',
   secret: process.env.SECRET || 'secret secret secret'
 };
